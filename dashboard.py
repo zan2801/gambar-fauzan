3f37ca27fa6d75c023ce98b64e9cab271686c434
zan2801 <akbarfauzan2801@gmail.com>
2025-10-11 13:53:55 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,7 +12,7 @@ import cv2
 @st.cache_resource
 def load_models():
     yolo_model = YOLO("model/FauzanAkbar_Laporan4.pt")  # Model deteksi objek
-    classifier = tf.keras.models.load_model("model/classifier_model.h5")  # Model klasifikasi
+    classifier = tf.keras.models.load_model("Fauzan Akbar_Laporan 2.h5")  # Model klasifikasi
     return yolo_model, classifier
 
 yolo_model, classifier = load_models()
