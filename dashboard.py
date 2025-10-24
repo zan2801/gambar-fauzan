fefc6bdd412c3681cf4baafb3dd97cc073e8ee05
zan2801 <akbarfauzan2801@gmail.com>
2025-10-24 14:37:29 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="SpaceVision AI", page_icon="🪐", layout="wide")
 
 BG_COLOR = "#05091a"     # Lebih gelap agar bintang kontras
 TEXT_COLOR = "#ffffff"
-TEXT_JUGA =  "#778899"
+TEXT_JUGA =  "#F5F5DC"
 
 # Gaya dasar halaman
 st.markdown(f"""
